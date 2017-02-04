a51367e6f48c14728a6854e9e81df4b156852bdc
TheBaconSpace <therobotto@gmail.com>
2017-02-04 15:02:17 +0000

    Added Version 1.2.0

--- bot.js
+++ bot.js
@@ -7,30 +7,31 @@ const token = 'BotToken1'
 const token2 = 'BotToken2'
 const token3 = 'BotToken3'
 const dnd = 'dnd'
+const sbt = 'SelfbotTracker '
 
 
 bot.on("ready", () => {
-    console.log(bot.user.username + " Selfbot Online!");
+    console.log(bot.user.username + sbt+" Online!");
     bot.user.setStatus(`dnd`);
-    console.log(bot.user.username + " Selfbot Set My Self to " + dnd);
+    console.log(bot.user.username +  + sbt+ " Set My Self to " + dnd);
     bot.user.setGame(" On " + bot.guilds.array().length + " Servers");
-    console.log(bot.user.username + " Selfbot is in " + bot.guilds.array().length + " Servers");
+    console.log(bot.user.username + " SelfbotTracker is in " + bot.guilds.array().length + " Servers");
 });
 bot.login(token);
 
 client.on("ready", () => {
-    console.log(client.user.username + " Selfbot Online!");
+    console.log(client.user.username  + sbt+ " Online!");
     client.user.setStatus(`dnd`);
-    console.log(client.user.username + " Selfbot Set My Self to " + dnd);
+    console.log(client.user.username  + sbt+ "  Set My Self to " + dnd);
     client.user.setGame(" On " + client.guilds.array().length + " Servers");
-    console.log(client.user.username + " Selfbot is in " + client.guilds.array().length + " Servers");
+    console.log(client.user.username  + sbt+"  is in " + client.guilds.array().length + " Servers");
 });
 client.login(token2);
 mybot.on("ready", () => {
-    console.log(mybot.user.username + " Selfbot Online!");
+    console.log(mybot.user.username  + sbt+"  Online!");
     mybot.user.setStatus(`dnd`);
-    console.log(mybot.user.username + " Selfbot Set My Self to " + dnd);
+    console.log(mybot.user.username  + sbt+"  Set My Self to " + dnd);
     mybot.user.setGame(" On " + mybot.guilds.array().length + " Servers");
-    console.log(mybot.user.username + " Selfbot is in " + mybot.guilds.array().length + " Servers");
+    console.log(mybot.user.username +  + sbt+"  is in " + mybot.guilds.array().length + " Servers");
 });
 mybot.login(token3);
